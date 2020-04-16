4c264ec623df6bb804888c848ff1db6c957c9fdb
Glitch (hello-webpage) <none>
2020-04-16 18:17:51 +0000

    🍎🐚 Checkpoint ./index.html:4782312/114 ./script.js:4782312/847

--- script.js
+++ script.js
@@ -6,3 +6,52 @@ for (let i = 0; i<20 ; i++){
   console.log(i);
 }
 console.log("hello");
+
+let x = 20; // set variables
+let y = 20;
+let spx =-1;
+let spy =-1;
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+  fill(255,120,12);
+  scale(10);
+  stroke(255,0,0);
+  point(20,24);
+  
+}
+
+function draw() {
+  //background(220);
+  loadPixels();  // use pixexl array 
+  x+= spx;
+  y+=spy;
+  nextpix1 = get(x,y);  // check if the pixel is turned on or off
+  nextpix2 = get(x+1,y); // need to check a few pixels 
+  nextpix3 = get (x, y+1);
+  
+  if (nextpix1[0]>0 || nextpix2[0]>0 || nextpix3[0]>0){
+   
+   x=random(width);   // if on set to a random postion on the canvas
+   
+   y=random(height)
+   if (random(1)>0.5){
+    spx*=-1; 
+   }else{
+    spy*=-1; 
+   }
+  }
+  
+   if (x > width || x<0){   // check edges
+    spx*=-1;
+  }
+  if (y > height || y< 0){
+    
+    spy*=-1;
+  }
+  
+  set(x,y,[255,255,0,255]);
+  updatePixels(); 
+    
+}
